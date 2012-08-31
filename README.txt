b0b85309c742ff09fc134a59a87361cd0ea28a8a
MURAOKA Yusuke <yusuke@jbking.org>
2012-08-31 13:50:45 +0000

    init

---
+++ README.txt
@@ -0,0 +1,14 @@
+proj README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $venv/bin/python setup.py develop
+
+- $venv/bin/populate_proj development.ini
+
+- $venv/bin/pserve development.ini
+
---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='proj',
+      version='0.0',
+      description='proj',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='proj',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = proj:main
+      [console_scripts]
+      initialize_proj_db = proj.scripts.initializedb:main
+      """,
+      )
+
